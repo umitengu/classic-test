05714cef9b5e87ff1973be93180838a31214433d
developer-min <ichigo.sk6@gmail.com>
2023-12-29 01:39:09 +0900

    Fixed build process.

--- app/src/main/java/com/example/classictestapp/MainActivity.kt
+++ app/src/main/java/com/example/classictestapp/MainActivity.kt
@@ -16,29 +16,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ClassicTestAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                    Greeting("Harapeco Aomushi")
-                }
-            }
+            Greeting("Harapeco Aomushi")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    ClassicTestAppTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ClassicTestAppTheme {
-        Greeting("Android")
+        Greeting("Harapeco Aomushi")
     }
 }
