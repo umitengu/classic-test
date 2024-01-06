08385e57acda99a0e5adffdc4b73305fe806e2ca
hiroaki_inokuchi <lippygames24@gmail.com>
2024-01-06 16:45:38 +0900

    ChatGPTのAPIを実行するスクリプトを追加

--- app/src/main/java/com/example/classictestapp/MainActivity.kt
+++ app/src/main/java/com/example/classictestapp/MainActivity.kt
@@ -6,37 +6,54 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.classictestapp.ui.theme.ClassicTestAppTheme
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Harapeco Aomushi")
+
+            RecipeApp()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    ClassicTestAppTheme {
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-    }
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ClassicTestAppTheme {
-        Greeting("Harapeco Aomushi")
+fun RecipeApp() {
+    val apiKey = "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"
+    val (text, setText) = remember { mutableStateOf("") }
+    val (response, setResponse) = remember { mutableStateOf("") }
+    val coroutineScope = rememberCoroutineScope()
+    Column(modifier = Modifier.padding(16.dp)) {
+        BasicTextField(
+            value = text,
+            onValueChange = setText,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Button(onClick = {
+            coroutineScope.launch {
+                // ここで非同期処理を行う
+                val result = NetworkService.getChatGptResponse(
+                    text,
+                    apiKey
+                )
+                setResponse(result)
+            }
+        }) {
+            Text("送信")
+        }
+        Text(response)
     }
 }
